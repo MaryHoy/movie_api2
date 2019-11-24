60174677cb86f4d381bc50a6750a5e5a8bac7e65
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-11-23 16:27:48 -0800

    added public folder

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const Movie = require("./movies");
+const Text = require("./text");
+let topMovies = [
+  {
+    title: "Avengers 1",
+  },
+  {
+    title: "Avengers 2",
+  },
+  {
+    title: "Avengers 3",
+  }
+];
+
+app.get("/index.html", function(req, res) {});
+app.get("/", function(req, res) {
+  res.send("Welcome to my app!");
+
+});
+app.get("/secreturl", function(req, res) {
+  res.send("This is a secret url with super top-secret content.");
+});
+
+app.get("/documentation", function(req, res) {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+app.get("/books", function(req, res) {
+  res.json(topMovies);
+});
+
+app.get("/movies", function(req, res) {
+  console.log(Movie);
+  res.json(Movie);
+});
+
+app.get("/text", function(req, res) {
+  res.send(Text);
+});
+
+app.listen(8080, () => console.log("Your app is listening on port 8080."));
---
+++ movies.js
@@ -0,0 +1,35 @@
+module.exports.movie = [
+  {
+    title: "Angers 1",
+  },
+  {
+    title: "Angers 2",
+  },
+  {
+    title: "Angers 3",
+  },
+  {
+    title: "Angers 4",
+  },
+  {
+    title: "Angers 5",
+  },
+  {
+    title: "Angers 6",
+  },
+  {
+    title: "Angers 7",
+  }
+];
+{
+  title: "Angers 8",
+}
+];
+{
+  title: "Angers 9",
+}
+];
+{
+  title: "Angers 10",
+}
+];
