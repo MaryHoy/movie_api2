3613c538d54638608a3f2b46a098c13a82f595ea
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-11-30 14:34:16 -0800

    added table to document.html

--- students.js
+++ students.js
@@ -8,44 +8,43 @@ app.use(bodyParser.json());
 
 let Students = [
   {
-    id: 1,
-    name: "Jessica Drake",
-    classes: {
-      biology: 95,
-      algebra: 92
+    id : 1,
+    name : "Jessica Drake",
+    classes : {
+      biology : 95,
+      algebra : 92
     }
   },
   {
-    id: 2,
-    name: "Ben Cohen",
-    classes: {
-      biology: 95,
-      algebra: 92
+    id : 2,
+    name : "Ben Cohen",
+    classes : {
+      biology : 95,
+      algebra : 92
     }
   },
   {
-    id: 3,
-    name: "Lisa Downing",
-    classes: {
-      biology: 95,
-      algebra: 92
+    id : 3,
+    name : "Lisa Downing",
+    classes : {
+      biology : 95,
+      algebra : 92
     }
   }
 ];
 
+// Gets the list of data about ALL students
+
 app.get("/students", (req, res) => {
   res.json(Students);
 });
-app.get("/students", (req, res) => {
- res.send(“Successful GET request returning data on all the students”);
-});
+// Gets the data about a single student, by name
+
 app.get("/students/:name", (req, res) => {
-  res.json(
-    Students.find(student => {
-      return student.name === req.params.name;
-    })
-  );
+  res.json(Students.find( (student) =>
+    { return student.name === req.params.name   }));
 });
+// Adds data for a new student to our list of students.
 app.post("/students", (req, res) => {
   let newStudent = req.body;
 
@@ -59,47 +58,31 @@ app.post("/students", (req, res) => {
   }
 });
 
+// Deletes a student from our list by ID
 app.delete("/students/:id", (req, res) => {
-  let student = Students.find(student => {
-    return student.id === req.params.id;
-  });
+  let student = Students.find((student) => { return student.id === req.params.id });
 
   if (student) {
-    Students.filter(function(obj) {
-      return obj.id !== req.params.id;
-    });
-    res.status(201).send("Student " + req.params.id + " was deleted.");
+    Students.filter(function(obj) { return obj.id !== req.params.id });
+    res.status(201).send("Student " + req.params.id + " was deleted.")
   }
 });
 
+// Update the "grade" of a student by student name/class name
 app.put("/students/:name/:class/:grade", (req, res) => {
-  let student = Students.find(student => {
-    return student.name === req.params.name;
-  });
+  let student = Students.find((student) => { return student.name === req.params.name });
 
   if (student) {
     student.classes[req.params.class] = req.params.grade;
-    res
-      .status(201)
-      .send(
-        "Student " +
-          req.params.name +
-          " was assigned a grade of " +
-          req.params.grade +
-          " in " +
-          req.params.class
-      );
+    res.status(201).send("Student " + req.params.name + " was assigned a grade of " + req.params.grade + " in " + req.params.class);
   } else {
-    res
-      .status(404)
-      .send("Student with the name " + req.params.name + " was not found.");
+    res.status(404).send("Student with the name " + req.params.name + " was not found.")
   }
 });
 
+// Gets the GPA of a student
 app.get("/students/:name/gpa", (req, res) => {
-  let student = Students.find(student => {
-    return student.name === req.params.name;
-  });
+  let student = Students.find((student) => { return student.name === req.params.name });
 
   if (student) {
     let classesArray = Object.keys(student.classes);
@@ -111,10 +94,11 @@ app.get("/students/:name/gpa", (req, res) => {
     console.log(classesArray.length);
     console.log(gpa);
     res.status(201).send("" + gpa);
-  } else {
-    res
-      .status(404)
-      .send("Student with the name " + req.params.name + " was not found.");
+    //res.status(201).send(gpa);
+  }
+  else
+  {
+    res.status(404).send("Student with the name " + req.params.name + " was not found.");
   }
 });
 
