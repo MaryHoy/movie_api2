5659e1eefcd3719efe923154cebf48ddd34c5215
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-12-16 20:03:42 -0800

    corrected typo

--- models.js
+++ models.js
@@ -3,27 +3,28 @@ var movieSchema = mongoose.Schema({
     Title : {type: String, required: true},
     Description : {type: String, required: true},
     Genre : {
-        Name : String
+      Name : String,
+      Description : String
     },
     Director : {
-        Name : String,
-        Bio : String
+      Name : String,
+      Bio : String
     },
     Actors : [String],
     ImagePath : String,
     Featured : Boolean
-});
-
-var usersSchema = mongoose.Schema({
-    Username : {type: String, required: true},
-    Password : {type: String, required: true},
-    Email : {type: String, required: true},
-    Birthday : Date,
-    FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Moive' }]
-});
-
-var Movie = mongoose.model('movie', movieSchema);
-var User = mongoose.model('User', userSchema);
-
-module.exports.Movie = Movie;
-module.exports.User = User;
+  });
+  
+  var userSchema = mongoose.Schema({
+   Username : {type: String, required: true},
+   Password : {type: String, required: true},
+   Email : {type: String, required: true},
+   Birthday : Date,
+   FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+  var Movie = mongoose.model('Movie', movieSchema);
+  var User = mongoose.model('User', userSchema);
+  
+  module.exports.Movie = Movie;
+  module.exports.User = User;
