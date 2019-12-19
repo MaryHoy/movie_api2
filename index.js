35b56dc9725058bf34b49c24c36817ecb433463a
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-12-19 01:10:59 -0800

    Update index.js
    added passport

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
-const Models = require('./models.js');
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const uuid = require('uuid');
+const mongoose = require("mongoose");
+const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
+const { check, validationResult } = require("express-validator");
+const passport = require("passport");
+require("./passport");
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 
-const express = require("express"),
-  morgan = require("morgan"),
-  bodyParser = require("body-parser"),
-  uuid = require("uuid");
-
-const app = express();
-
-app.use(bodyParser.json());
+var auth = require('./auth')(app);
 
 // Return a list of ALL movies to the user
 app.get('/movies', function(req , res){ 
@@ -129,7 +130,7 @@ app.delete('/users/:Username', function(req, res) {
   (required)
   Birthday: Date
 }*/
-app.put('/users/:Username', function(req, res) {
+app.put('/users', function(req, res) {
   Users.findOneAndUpdate({ Username : req.params.Username }, { $set :
   {
     Username : req.body.Username,
@@ -147,6 +148,47 @@ app.put('/users/:Username', function(req, res) {
     }
   })
 });
+
+
+
+
+app.post('/users', [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
+  var errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).json({errors: errors.array()});
+  }
+  var hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({Username: req.body.Username})
+  .then(function(user){
+    if(user){
+      return res.status(400).send(req.body.Username + " already exists")
+    }else{
+      Users.create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then(function(user){res.status(201).json(user)})
+        .catch(function(error){
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        })
+      }
+    }).catch(function(error){
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+  });
+
+
+
+
 
 // Allow users to add a movie to their list of favorites
 app.post('/users/:Username/Movies/:MovieID', function(req, res) {
