82a5df0e93714a0ebde526453dc71ff15a8eb0dc
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-01-22 13:31:27 -0800

    update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,21 @@
-import React, {Component} from 'react';
+// client/src/index.jsx
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MainView from './components/main-view/main-view';
+import { MainView } from './components/main-view/main-view';
 
+// Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp);
-
-class MyFlixApp extends Component {
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
-        <MainView />
-      </Provider>
-    ); 
+    return <MainView/>;
   }
 }
 
-// Finds the root of your app
+// Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApp), container);
+// Tell React to render our app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
