779d9283efb413cf819cc17ebbff969331ce9016
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-02-08 15:39:36 -0800

    fixing heroku

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -41451,7 +41451,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58933" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55932" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
